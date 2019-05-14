424d5005caba07815e329c0b91eab5126854792a
Vladimir <vandrienko1991@gmail.com>
2019-05-14 15:00:14 +0300

    Собрал SVG-спрайт

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var csso = require("gulp-csso"); //Минифицируем CSS
 var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin"); // Оптимизируем изображения
 var webp = require("gulp-webp"); //Создаём WebP-изображение
+var svgstore = require("gulp-svgstore"); //Соберём SVG-спрайт
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
     .pipe(plumber())
@@ -59,3 +60,13 @@ gulp.task("webp", function () {
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest("source/img"));
   });
+
+//Соберём SVG-спрайт
+gulp.task("sprite", function () {
+  return gulp.src("source/img/icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"));
+});
