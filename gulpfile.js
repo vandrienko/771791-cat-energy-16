a75bc129978928a1009a67db6b65d984ead39bbf
Vladimir <vandrienko1991@gmail.com>
2019-05-15 00:22:19 +0300

    локальный сервер не работает

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ var webp = require("gulp-webp"); //Создаём WebP-изображение
 var svgstore = require("gulp-svgstore"); //Соберём SVG-спрайт
 var posthtml = require("gulp-posthtml");  //Для добавления svg в разметку
 var include = require("posthtml-include");
+var del = require("del"); //Для удаления папки
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -26,24 +27,26 @@ gulp.task("css", function () {
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
+    server: "build/"
   });
 
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/less/**/*.less", gulp.series("css"));
+  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("start", gulp.series("build", "server"));
 
 // Оптимизация изображений
 gulp.task("images", function () {
@@ -71,7 +74,7 @@ gulp.task("sprite", function () {
       inlineSvg: true
     }))
     .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("build/img"));
 });
 
 //Для добавления svg в разметку
@@ -80,5 +83,32 @@ gulp.task("html", function () {
   .pipe(posthtml([
     include()
   ]))
-  .pipe(gulp.dest("source"));
+  .pipe(gulp.dest("build"));
 });
+
+// Копируем файлы в папку build
+gulp.task("copy", function () {
+  return gulp.src([
+  "source/fonts/**/*.{woff,woff2}",
+  "source/img/**",
+  "source/js/**",
+  "source/*.ico"
+  ], {
+  base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+
+// Очищаем папку build перед копированием
+gulp.task("clean", function () {
+  return del("build");
+});
+
+// запуск билда
+gulp.task("build", gulp.series(
+  "clean",
+  "copy"
+  // "css",
+  // "sprite",
+  // "html"
+  ));
